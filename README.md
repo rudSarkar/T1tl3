ed614df46cefe53855244802f635c3dc7fd7a4c6
Rudra Sarkar <rudrasarkar815@gmail.com>
2019-06-19 04:53:11 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# T1tl3
+
+T1tl3 is a simple tool that can grab URLs/Subdomains `title` , Actaully this is a personal tool but i thought let's make it public.I know T1tl3 don't have any special fetaures, But still it cool for me 😁 . The cool fact is T1tl3 supports **Python 2** and **Python 3** as well as.
+
+# Install / Usage
+
+**Install Dependencies:**
+
+```
+$ pip install -r requirements.txt
+```
+
+**Usage**
+
+```
+$ python T1tl3.py urlList.txt
+```
+
+Ping me if you having problem with this tool [🐦 @rudr4_sarkar](https://twitter.com/rudr4_sarkar)
---
+++ T1tl3.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Rudra Sarkar
+# Twitter: @rudr4_sarkar
+# T1tl3.py
+
+import sys
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+def usage():
+	print ('')
+	print ('$ python ' + sys.argv[0] + ' urlList.txt')
+
+def banner():
+	print ('''
+	  _______   __   _     _   ____  
+	 |__   __| /_ | | |   | | |___ \ 
+	    | |     | | | |_  | |   __) |
+	    | |     | | | __| | |  |__ < 
+	    | |     | | | |_  | |  ___) |
+	    |_|     |_|  \__| |_| |____/ 
+
+	    By Rudra Sarkar, Twitter: @rudr4_sarkar                          
+	''')
+
+def checkTitle(argv):
+	file = argv
+	with open(file) as f:
+		for line in f:
+			try:
+				line2 = line.strip()
+				url = 'https://'+line2
+				try:
+					result = requests.get(url)
+					soup = bs(result.content, 'lxml').encode("utf-8")
+					title = soup.select_one('title').text
+					print ('')
+					print (' [+] URL Title: ' + title)
+					print (' [+] URL: ' + url)
+					print (' [+] URL Status: ' + result.status_code)
+				except:
+					print ('')
+					print (' [x] No title found')
+					print (' [x] URL: ' + url)
+					print (' [x] URL Status: ' + result.status_code)
+			except:
+				print (' [x] Error')
+				
+if (len(sys.argv)) == 2:
+	try:
+		print ('')
+		banner()
+		print ('')
+		print (' [+] Scanning start ...')
+		checkTitle(sys.argv[1])
+	except:
+		print ('')
+		print (' [x] Can\'t find your ' + sys.argv[1] + ' file.')
+else:
+	usage()
+	sys.exit(0)
